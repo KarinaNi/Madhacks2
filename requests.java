a2dc81660b0e809dadcd2013014607cf4862f63a
Shrehit <32557313+Shrehit@users.noreply.github.com>
2017-10-07 15:44:54 -0500

    Add files via upload

--- requests.java
+++ requests.java
@@ -19,45 +19,51 @@ import org.apache.http.message.BasicNameValuePair;
 
 
 public class requests {
-	private final String USER_AGENT = "Mozilla/5.0";
-	public static void main(String[] args) throws Exception{
-		requests http = new requests();
-		System.out.println("Testing get");
-		http.get();
-		System.out.println("Testing send");
-		http.send();
-	}
-	private void get() throws Exception{
+        private final String USER_AGENT = "Mozilla/5.0";
+        public static void main(String[] args) throws Exception{
+            String key[][]=null;
+            String urlStart=null;
+            requests http = new requests();
+            System.out.println("Testing get");
+            http.get(key,urlStart);
+            System.out.println("Testing send");
+            http.send();
+        }
+        public String helper(String key[][],String url)
+        {
+            String finalURL = url+"?";
+            for(String row[]:key)
+            {
+                finalURL=finalURL+row[0]+"="+row[1]+"&";
+            }
+            finalURL=finalURL.substring(0, finalURL.length()-1);
+            return finalURL;
+        }
+        private void get(String key[][], String urlStart) throws Exception{
 
+            URL url = new URL(helper(key,urlStart));
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", USER_AGENT);
+            con.setRequestProperty("accept","application/json");
+            int responseCode = con.getResponseCode();
+            System.out.println("\nSending 'GET' request to URL : " + url);
+            System.out.println("Response Code : " + responseCode);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
 
-		//url
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            //print result
+            System.out.println(response.toString());
+            
+        }
+        public void send() throws Exception{
+            
+        }
+    }
 
-		//user: "bob"
-		//son: "joe"
-		//url?user=bob&son=joe
-		
-		URL url = new URL("https://open-ic.epic.com/FHIR/api/FHIR/DSTU2/Patient/Tbt3KuCY0B5PSrJvCu2j-PlK.aiHsu2xUjUM8bWpetXoB");
-		HttpURLConnection con = (HttpURLConnection)url.openConnection();
-		con.setRequestMethod("GET");
-		con.setRequestProperty("User-Agent", USER_AGENT);
-		con.setRequestProperty("accept","application/json");
-		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'GET' request to URL : " + url);
-		System.out.println("Response Code : " + responseCode);
-		BufferedReader in = new BufferedReader(
-		        new InputStreamReader(con.getInputStream()));
-		String inputLine;
-		StringBuffer response = new StringBuffer();
-
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
-		in.close();
-		//print result
-		System.out.println(response.toString());
-		
- 	}
-	public void send() throws Exception{
-		
-	}
-}
